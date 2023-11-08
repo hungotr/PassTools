67516e5bdfe5e8d57a639c4126985fb557bd7547
missn0body <hulk128t@gmail.com>
2023-11-08 13:14:24 -0800

    Added documentation and made code a little bit more clean

--- app/src/main/java/com/example/passtools/Key.java
+++ app/src/main/java/com/example/passtools/Key.java
@@ -7,14 +7,33 @@ public class Key extends EncodeDecode
     private Base64.Encoder b64En;
     private Base64.Decoder b64De;
 
-    public Key(String input)
-    {
-        super(input);
+    /**
+     * A less-specialized constructor for Key, meant as the
+     * default constructor for this object, with the mode
+     * again defaulting to encode mode. In fact, the body
+     * of this function essentially funnels the parameters
+     * into the overloaded constructor below, because Java
+     * is stupid and doesn't understand default parameters.
+     * <p>
+     * @author  Alex Sanchez
+     * @param   input           The string to be inputted as
+     *                          plaintext.
+     * */
+    public Key(String input) { this(input, EncodeDecode.Mode.ENCODE); }
 
-        this.b64En = Base64.getEncoder();
-        this.b64De = Base64.getDecoder();
-    }
-
+    /**
+     * A more-specialized constructor for Key, really only
+     * to be realistically used in the event of decoding
+     * a Key object, but still available nonetheless.
+     * <p>
+     * @author  Alex Sanchez
+     * @param   input           The string to be inputted, either
+     *                          plaintext or ciphertext
+     * @param   what            Enumerated type specifying what mode
+     *                          to put the underlying EncodeDecode structure
+     *                          into, as well as specifying what kind of
+     *                          input is being received
+     * */
     public Key(String input, Mode what)
     {
         super(input, what);
@@ -23,25 +42,61 @@ public class Key extends EncodeDecode
         this.b64De = Base64.getDecoder();
     }
 
+    /**
+     * An override of the EncodeDecode method encode()
+     * with this specific implementation being used to encode
+     * plaintext into a Base64 ciphertext, using Java 8's new
+     * default Base64 encoder.
+     * <p>
+     * @author  Alex Sanchez
+     * @return  a String object representing the newly made ciphertext
+     * */
     @Override
     public String encode()
     {
-        if(this.plainbytes == null) this.plainbytes = this.plaintext.getBytes(StandardCharsets.UTF_8);
+        try
+        {
+            if (this.plainbytes == null)
+                this.plainbytes = this.plaintext.getBytes(StandardCharsets.UTF_8);
 
-        String buf = new String(this.b64En.encode(this.plainbytes));
-        if(getCiphertext().isEmpty()) this.ciphertext = buf;
+            String buf = new String(this.b64En.encode(this.plainbytes));
+            if (getCiphertext().isEmpty()) this.ciphertext = buf;
 
-        return this.ciphertext;
+            return this.ciphertext;
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error in Key:encode()");
+            e.printStackTrace();
+            return null;
+        }
     }
 
+    /**
+     * An override of the EncodeDecode method decode()
+     * with this specific implementation being used to decode
+     * Base64 text.
+     * <p>
+     * @author  Alex Sanchez
+     * @return  a String object representing the newly made plaintext
+     * */
     @Override
     public String decode()
     {
-        if(this.cipherbytes == null) this.cipherbytes = this.ciphertext.getBytes(StandardCharsets.UTF_8);
+        try {
+            if (this.cipherbytes == null)
+                this.cipherbytes = this.ciphertext.getBytes(StandardCharsets.UTF_8);
 
-        String buf = new String(this.b64De.decode(this.cipherbytes));
-        if(getPlaintext().isEmpty()) this.plaintext = buf;
+            String buf = new String(this.b64De.decode(this.cipherbytes));
+            if (getPlaintext().isEmpty()) this.plaintext = buf;
 
-        return this.plaintext;
+            return this.plaintext;
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error in Key:decode()");
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- app/src/main/java/com/example/passtools/Password.java
+++ app/src/main/java/com/example/passtools/Password.java
@@ -1,80 +1,175 @@
 package com.example.passtools;
 
 import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 
 public class Password extends EncodeDecode
 {
+    final private String defaultKey = "BLOWFISHENCRYPT";
+    final private String method = "Blowfish";
+
     private Cipher cObj;
-    private Key bfKey = null;
+    private Key bfKey = new Key(this.defaultKey);
     private SecretKeySpec keyspec;
 
-    public Password(String input) throws NoSuchPaddingException, NoSuchAlgorithmException
+    /**
+     * A default constructor for the Password object, which
+     * really just funnels its parameters into a more specialized
+     * overloaded constructor below.
+     * <p>
+     * @author  Alex Sanchez
+     * @param   input           String object to be inputted representing
+     *                          plaintext
+     * */
+    public Password(String input)
     {
-        super(input);
-        this.cObj = Cipher.getInstance("Blowfish");
-
-        if(this.bfKey.getPlaintext().isEmpty() || this.bfKey == null)
-            this.bfKey = new Key("BLOWFISHENCRYPT");
-
-        this.keyspec = new SecretKeySpec(this.bfKey.plainbytes, "Blowfish");
+        this(input, Mode.ENCODE);
     }
 
-    public Password(String input, Mode what) throws NoSuchPaddingException, NoSuchAlgorithmException
+    /**
+     * A more specialized constructor which specifies the kind
+     * of mode to put the underlying EncodeDecode structure into,
+     * which also specifies the kind of input it is receiving.
+     * <p>
+     * @author  Alex Sanchez
+     * @param   input           String object to be inputted, either
+     *                          being plaintext or ciphertext depending
+     *                          on the mode given
+     *
+     * @param   what            Enumerated type representing the kind of
+     *                          mode to set this instance of the object
+     *                          into, as well as specifying what input
+     *                          is to be expected
+     * */
+    public Password(String input, Mode what)
     {
         super(input, what);
-        this.cObj = Cipher.getInstance("Blowfish");
-
-        if(this.bfKey.getPlaintext().isEmpty() || this.bfKey == null)
-            this.bfKey = new Key("BLOWFISHENCRYPT");
-
-        this.keyspec = new SecretKeySpec(this.bfKey.plainbytes, "Blowfish");
+        try
+        {
+            this.cObj = Cipher.getInstance(this.method);
+            this.keyspec = new SecretKeySpec(this.bfKey.plainbytes, this.method);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error in Password:constructor()");
+            e.printStackTrace();
+        }
     }
 
-    public Password(String input, String key) throws NoSuchPaddingException, NoSuchAlgorithmException
+    /**
+     * Another default constructor for the Password object, which
+     * really just funnels its parameters into a more specialized
+     * overloaded constructor below.
+     * <p>
+     * @author  Alex Sanchez
+     * @param   input           String object to be inputted representing
+     *                          plaintext
+     *
+     * @param   key             String object to be inputted into this
+     *                          Password's instance of a Key object
+     * */
+    public Password(String input, String key)
     {
-        super(input);
-        this.bfKey = new Key(key);
-        this.cObj = Cipher.getInstance("Blowfish");
-        this.keyspec = new SecretKeySpec(this.bfKey.plainbytes, "Blowfish");
+        this(input, key, Mode.ENCODE);
     }
 
-    public Password(String input, String key, Mode what) throws NoSuchPaddingException, NoSuchAlgorithmException
+    /**
+     * A more specialized constructor which specifies the kind
+     * of mode to put the underlying EncodeDecode structure into,
+     * which also specifies the kind of input it is receiving.
+     * <p>
+     * @author  Alex Sanchez
+     * @param   input           String object to be inputted, either
+     *                          being plaintext or ciphertext depending
+     *                          on the mode given
+     *
+     * @param   key             String object to be inputted into this
+     *                          Password's instance of a Key object
+     *
+     * @param   what            Enumerated type representing the kind of
+     *                          mode to set this instance of the object
+     *                          into, as well as specifying what input
+     *                          is to be expected
+     * */
+    public Password(String input, String key, Mode what)
     {
         super(input, what);
-        this.bfKey = new Key(key);
-        this.cObj = Cipher.getInstance("Blowfish");
-        this.keyspec = new SecretKeySpec(this.bfKey.plainbytes, "Blowfish");
+        try
+        {
+            this.bfKey = new Key(key);
+            this.cObj = Cipher.getInstance(this.method);
+            this.keyspec = new SecretKeySpec(this.bfKey.plainbytes, this.method);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error in Password:constructor(Mode)");
+            e.printStackTrace();
+        }
     }
 
+    /**
+     * An override of the EncodeDecode method encode()
+     * with this specific implementation being used to encode
+     * plaintext into ciphertext using the Blowfish cipher created
+     * by Bruce Schneier in 1993, which he has graciously released
+     * into the public domain.
+     * <p>
+     * @author  Alex Sanchez
+     * @return  a String object representing the newly made ciphertext
+     * */
     @Override
-    public String encode() throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException
+    public String encode()
     {
         if(this.plainbytes == null) this.plainbytes = this.plaintext.getBytes(StandardCharsets.UTF_8);
 
-        this.cObj.init(Cipher.ENCRYPT_MODE, keyspec);
-        String buf = new String(this.cObj.doFinal(this.plainbytes));
-        if(getCiphertext().isEmpty()) this.ciphertext = buf;
+        String buf;
+        try
+        {
+            this.cObj.init(Cipher.ENCRYPT_MODE, keyspec);
+            buf = new String(this.cObj.doFinal(this.plainbytes));
+            if (getCiphertext().isEmpty()) this.ciphertext = buf;
 
-        return buf;
+            return buf;
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error in Password:encode()");
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
+    /**
+     * An override of the EncodeDecode method decode()
+     * with this specific implementation being used to decode
+     * Blowfish ciphertext.
+     * <p>
+     * @author  Alex Sanchez
+     * @return  a String object representing the newly made plaintext
+     * */
     @Override
-    public String decode() throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException
+    public String decode()
     {
         if(this.plainbytes == null) this.plainbytes = this.plaintext.getBytes(StandardCharsets.UTF_8);
 
-        this.cObj.init(Cipher.DECRYPT_MODE, keyspec);
-        String buf = new String(this.cObj.doFinal(this.plainbytes));
-        if(getCiphertext().isEmpty()) this.ciphertext = buf;
+        String buf;
+        try
+        {
+            this.cObj.init(Cipher.DECRYPT_MODE, keyspec);
+            buf = new String(this.cObj.doFinal(this.plainbytes));
+            if (getCiphertext().isEmpty()) this.ciphertext = buf;
 
-        return buf;
+            return buf;
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error in Password:decode()");
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
