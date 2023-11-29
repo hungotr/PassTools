e6de36ef87da167599f43660ea83739f172cd668
Hunter Biddington <145161217+hungotr@users.noreply.github.com>
2023-11-29 08:20:49 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/passtools/MainActivity.java
+++ app/src/main/java/com/example/passtools/MainActivity.java
@@ -11,24 +11,47 @@ import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Button openTest;
+    private Button openTest, openGen,openStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         openTest = (Button) findViewById(R.id.test_button);
+        openGen =(Button) findViewById(R.id.make_button);
+        openStore = (Button) findViewById(R.id.store_button);
+        openGen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGenActivity();
+            }
+        });
         openTest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openTestActivity();
             }
         });
+        openStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openStoreActivity();
+            }
+        });
 
+    }
+
+    void openGenActivity(){
+        Intent intent = new Intent(this,GenActivity.class);
+        startActivity(intent);
     }
 
     void openTestActivity(){
         Intent intent = new Intent(this,TestActivity.class);
+        startActivity(intent);
+    }
+    void openStoreActivity(){
+        Intent intent = new Intent(this,RecallActivity.class);
         startActivity(intent);
     }
 }
